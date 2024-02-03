1baead6cf89c6174e4defa72c2636efbffdb7042
jeff.li <lijippy@163.com>
2024-02-03 18:28:48 +0800

    add conda build

---
+++ conda/bld.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# Copyright 2023 The Carbin Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+set -e
+
+mkdir build
+cd build
+cmake ../llvm -G Ninja \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DLLVM_INCLUDE_TESTS=OFF \
+    -DLLVM_ENABLE_RTTI=ON \
+    -DLLVM_ENABLE_ZLIB=OFF \
+    -DLLVM_ENABLE_TERMINFO=OFF \
+    -DLLVM_TARGETS_TO_BUILD=all
+
+cmake --build .
+cmake --build . --target install
